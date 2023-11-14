ca99ab4107778e56b37e31d5ecedd368b961fc8f
electrovir <electrovir@users.noreply.github.com>
2023-11-14 14:18:26 +0000

    [minor] add AllDurations

--- src/duration.ts
+++ src/duration.ts
@@ -1,5 +1,4 @@
 import {RequiredAndNotNull} from '@augment-vir/common';
-import {DurationObjectUnits} from 'luxon';
 
 export enum DurationUnit {
     Years = 'years',
@@ -14,8 +13,28 @@ export enum DurationUnit {
     Milliseconds = 'milliseconds',
 }
 
-/** All possible options for the more strict Duration type. */
-export type AnyDuration = DurationObjectUnits;
+/**
+ * All possible options for the more strict Duration type. Matches the DurationObjectUnits type from
+ * the luxon package.
+ */
+export type AnyDuration = {
+    years?: number | undefined;
+    quarters?: number | undefined;
+    months?: number | undefined;
+    weeks?: number | undefined;
+    days?: number | undefined;
+    hours?: number | undefined;
+    minutes?: number | undefined;
+    seconds?: number | undefined;
+    milliseconds?: number | undefined;
+};
+
+/**
+ * All possible duration units at once. Whether these duration units should be added together to
+ * form the whole picture or whether each duration is the same value but calculated in different
+ * units depends on the context of this type's usage.
+ */
+export type AllDurations = RequiredAndNotNull<AnyDuration>;
 
 /**
  * Not a single date instance, but a description of a date duration. Used to calculate diffs between
@@ -23,6 +42,4 @@ export type AnyDuration = DurationObjectUnits;
  * property is set on this at any given time.
  */
 export type Duration<DurationKeys extends DurationUnit = DurationUnit> =
-    DurationUnit extends DurationKeys
-        ? AnyDuration
-        : Pick<RequiredAndNotNull<AnyDuration>, `${DurationKeys}`>;
+    DurationUnit extends DurationKeys ? AnyDuration : Pick<AllDurations, `${DurationKeys}`>;
